21271c3059ef453d4268471c8db95eb940093292
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2009-01-22 13:31:35 +0000

    .NET 2.0 用と 3.5 用に分けていたバッチファイルをひとつにまとめる

--- BuildReleaseX86.cmd
+++ BuildReleaseX86.cmd
@@ -1,1 +1,16 @@
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe /p:Configuration=Release /p:Platform=x86 GExplorer.sln
+@echo off
+setlocal
+
+set conf=Release
+set v35msbuild=%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe
+set v20msbuild=%windir%\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe
+
+if exist %v35msbuild% (
+	%v35msbuild% /p:Configuration=%conf% /p:Platform=x86 GExplorer.sln
+) else if exist %v20msbuild% (
+	%v20msbuild% /p:Configuration=%conf% /p:Platform=x86 GExplorer\GExplorer.csproj
+) elseÅ@(
+	echo MSBuild Ç™å©Ç¬Ç©ÇÁÇ»Ç©Ç¡ÇΩ
+)
+
+endlocal
--- BuildReleaseX86_Obsolete.cmd
+++
@@ -1,1 +0,0 @@
-%windir%\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe /p:Configuration=Release /p:Platform=x86 GExplorer\GExplorer.csproj
